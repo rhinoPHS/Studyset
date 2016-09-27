d9f58bfdca07ae65dfe0349514fea17b7f241f54
cord7894 <cord7894@naver.com>
2016-09-27 12:11:41 +0900

    JustJava

--- JustJava/app/src/main/java/com/example/a/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/a/justjava/MainActivity.java
@@ -3,36 +3,54 @@ package com.example.a.justjava;
 /**
  * Add your package below. Package name can be found in the project's AndroidManifest.xml file.
  * This is the package name our example uses:
- *
+ * <p>
  * package com.example.android.justjava;
  */
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.telecom.Connection;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
+
     int quantity = 2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
     /**
      * This method is called when the plus button is clicked.
      */
     public void increment(View view) {
+        if (quantity == 100) {
+            Toast.makeText(this,"You cannot have more than 100 cofee",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
         quantity = quantity + 1;
         display(quantity);
     }
 
     public void decrement(View view) {
-        quantity = quantity -1;
+        if (quantity == 1) {
+            Toast.makeText(this,"You cannot have less than 1 cofee",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        quantity = quantity - 1;
         display(quantity);
     }
 
@@ -40,20 +58,50 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        CheckBox whippedCreamCheckBox = (CheckBox)findViewById(R.id.checkBox_Whipped_cream);
+
+        EditText nameField = (EditText) findViewById(R.id.name_field);
+        String name = nameField.getText().toString();
+
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.checkBox_Whipped_cream);
         boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
 
-        int price = calculatePrice();
-        String priceMessage = createOrderSummary(price,hasWhippedCream);
-        displayMessage(priceMessage);
+        CheckBox ChocolateCheckBox = (CheckBox) findViewById(R.id.checkBox_Chocolate);
+        boolean hasChocolate = ChocolateCheckBox.isChecked();
 
+        int price = calculatePrice(hasWhippedCream, hasChocolate);
+        String priceMessage = createOrderSummary(name, price, hasWhippedCream, hasChocolate);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Just Java order for " + name);
+        intent.putExtra(Intent.EXTRA_TEXT,priceMessage);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
+        displayMessage(priceMessage);
     }
+
     /**
      * Calculates the price of the order.
+     *
+     * @param addWihppedCream is whether or not the user wants whipped cream topping
+     * @param addChocolate    is whether or not the user wants chocolate topping
+     * @return total price
      */
-    private int calculatePrice() {
-        int price = quantity * 5;
-        return  price;
+    private int calculatePrice(boolean addWihppedCream, boolean addChocolate) {
+        //Price of cup of coffee
+        int basePrice = 5;
+
+        // Add $1 the user wants whipped cream
+        if (addWihppedCream) basePrice++;
+
+        // Add $2 the user wants chocolate
+        if (addChocolate) basePrice += 2;
+
+        // Calculate the total order price by multiplying
+        return quantity * basePrice;
     }
 
     /*
@@ -62,13 +110,14 @@ public class MainActivity extends AppCompatActivity {
      * @return text summary
      */
 
-    private String createOrderSummary(int price, boolean addWippedCream){
+    private String createOrderSummary(String name, int price, boolean addWippedCream, boolean addChoco) {
         String summaryMessage;
-        summaryMessage = "Name : Kaptain Kunal\n";
+        summaryMessage = "Name : " + name + "\n";
         summaryMessage += "Add whipped cream? " + addWippedCream + "\n";
-        summaryMessage += "Quantity : "+quantity + "\n";
-        summaryMessage +=  "Total $" + price + "\n";
-        summaryMessage +=  "Thank you! ";
+        summaryMessage += "Add Chocolate? " + addChoco + "\n";
+        summaryMessage += "Quantity : " + quantity + "\n";
+        summaryMessage += "Total $" + price + "\n";
+        summaryMessage += "Thank you! ";
         return summaryMessage;
     }
 
--- PHS/app/src/main/java/com/example/sh408/myapplication/UserInterFace/TextView/gravity.java
+++ PHS/app/src/main/java/com/example/sh408/myapplication/UserInterFace/TextView/gravity.java
@@ -1,3 +1,9 @@
+/*
+문자의 배치 변경하기
+문자를 왼쪽, 가운데, 오른쪽, 정렬
+문자를 위쪽, 가운데, 아래쪽에 배치
+ */
+
 package com.example.sh408.myapplication.UserInterFace.TextView;
 
 import android.support.v7.app.AppCompatActivity;
@@ -5,11 +11,11 @@ import android.os.Bundle;
 
 import com.example.sh408.myapplication.R;
 
-public class Main2Activity extends AppCompatActivity {
+public class gravity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_gravity);
     }
 }
