f8d363780f6f331b21c52a1ec9d4b83935373cc5
rhinoPHS <cord7894@naver.com>
2016-10-03 12:20:27 +0900

    Lab45

---
+++ Lab45/app/src/main/java/com/example/sh408/lab45/Lab5.java
@@ -0,0 +1,13 @@
+package com.example.sh408.lab45;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Lab5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab5);
+    }
+}
